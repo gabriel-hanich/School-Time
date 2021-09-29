0c4ae9a4996cdc0268c55c237620ac7bf2c352d1
Gabriel-Hanich <gabriel.hanich1@gmail.com>
2021-09-29 14:20:49 +1000

    Gathered data from iCal file and created class structure to store it in

--- scripts/upload.js
+++ scripts/upload.js
@@ -16,7 +16,16 @@ submitButton.addEventListener("click", function(event){
  
 });
 
+class periodClass{
+    constructor(startDate, endDate, desc, summary, location){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.desc = desc;
+        this.summary = summary;
+        this.location = location
 
+    }
+}
 
 function processData(data){
     var dataList = [];
@@ -29,35 +38,48 @@ function processData(data){
             }
         }
     }
+
+    var periodClassList = [];
     for(var i = 0; i<dataList.length; i++){
-        if(dataList[i].includes("DTSTART")){
-            var year = dataList[i].substring(24, 28);
+        if (dataList[i] == "BEGIN:VEVENT"){ // BEGUIN:VEVENT occurs at the start of new period on the iCal file
+            // Calculate starting date and time of period
+            var startYear = dataList[i + 1].substring(24, 28);
 
-            var month = parseInt(dataList[i].substring(28, 30));
-            month -= 1;
-            month = month.toString();
-            var day = dataList[i].substring(30, 32);
+            var startMonth = parseInt(dataList[i + 1].substring(28, 30));
+            startMonth -= 1;
+            startMonth = startMonth.toString();
+            var startDay = dataList[i + 1].substring(30, 32);
 
-            var hour = dataList[i].substring(33, 35);
-            var minute = dataList[i].substring(35, 37);
-
-            var startDate = new Date(Date.UTC(year, month, day, hour, minute ,0));
-            console.log(startDate)
+            var startHour = dataList[i + 1].substring(33, 35);
+            var startMinute = dataList[i+ 1].substring(35, 37);
             
-        }
-    else if(dataList[i].includes("DTEND")){
-        var year = dataList[i].substring(22, 26);
+            var startDate = new Date(Date.UTC(startYear, startMonth, startDay, startHour, startMinute ,0));
+            // Calculate end date and time of period
+            var endYear = dataList[i + 3].substring(22, 26);
 
-        var month = parseInt(dataList[i].substring(26, 28));
-        month -= 1;
-        month = month.toString();
-        var day = dataList[i].substring(28, 30);
+            var endMonth = parseInt(dataList[i + 3].substring(26, 28));
+            endMonth -= 1;
+            endMonth = endMonth.toString();
+            var endDay = dataList[i + 3].substring(28, 30);
 
-        var hour = dataList[i].substring(31, 33);
-        var minute = dataList[i].substring(33, 35);
+            var endHour = dataList[i + 3].substring(31, 33);
+            var endMinute = dataList[i + 3].substring(33, 35);
 
-        var endDate = new Date(Date.UTC(year, month, day, hour, minute ,0));
-        console.log(endDate);
+
+            var endDate = new Date(Date.UTC(endYear, endMonth, endDay, endHour, endMinute ,0));
+            
+
+            // Get other pieces of data
+            var desc = dataList[i + 5].substring(12);  
+            var summary = dataList[i + 6].substring(8);
+            var location = dataList[i + 7].substring(9)
+
+            if(dataList[i+ 8] == "END:VEVENT"){ // Double checks file strucrture before appending list
+                periodClassList.push(new periodClass(startDate, endDate, desc, summary, location));
+            }
+
         }
     }
+
+    console.log(periodClassList)
 }
