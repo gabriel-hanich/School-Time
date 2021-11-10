c3e8cf78fc52d5bfc11522eb1e5e54c7f3dd12d1
Gabriel-Hanich <gabriel.hanich1@gmail.com>
2021-11-10 18:57:12 +1100

    Updated settings section
    HDJIKSgfajhkgadfhjk it finally builds

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,51 +1,56 @@
-var settingsCards = document.getElementsByClassName("settingsCard");
-var currentCard = 0;
-var lastCard = 1
-var leftArrow = document.getElementById("leftArrow");
+var settingsCardsRaw = document.getElementsByClassName("settingsCard");
 var rightArrow = document.getElementById("rightArrow");
+var leftArrow = document.getElementById("leftArrow");
 
-leftArrow.classList.add("greyed")
+var currentCardIndex = 0
 
-function updateCards(){
-    if(currentCard - lastCard == 1){
-        settingsCards[currentCard - 1].style = `
-            animation-name: move;
-            animation-duration: 250ms;
-            animation-timing-function: linear;
+var settingsCards = [];
+for(var i=0; i<settingsCardsRaw.length; i++){
+    settingsCards.push(settingsCardsRaw[i])
+}
+
+settingsCards[currentCardIndex].style = `
+    z-index: 3
+`
+
+function moveCards(change){
+    var currentCard = settingsCards[currentCardIndex % 3]
+    if((currentCardIndex % 3) + 1 == settingsCards.length){
+        var nextCard = settingsCards[0]
+    }else{
+        var nextCard = settingsCards[(currentCardIndex % 3) + 1]
+    }
+    
+    console.log(currentCard)
+    console.log(nextCard)
+    for(var i=0; i<settingsCards.length; i++){
+        settingsCards[i].style = `
+            z-index:1;
         `
     }
 
-
+    currentCard.style = `
+        z-index: 4;
+        animation-name: moveLeftOff;
+        animation-duration: 500ms;
+    `
+    nextCard.style = `
+        z-index: 2
+    `
+    
     setTimeout(() => {
-        for(var i=0; i<settingsCards.length; i++){
-            settingsCards[i].classList.add("hidden")
-        }
-        settingsCards[currentCard].classList.remove("hidden")
-    }, 250);
+        console.log("HIDDEN")
+        currentCard.style = `
+            z-index:1;
+        `
+        currentCardIndex += change
+    }, 500);
 
-    leftArrow.classList.remove("greyed")
-    rightArrow.classList.remove("greyed")
-
-    if(currentCard == 0){
-        leftArrow.classList.add("greyed")
-    }else if(currentCard == settingsCards.length - 1){
-        rightArrow.classList.add("greyed")
-    }
 }
-
-
-leftArrow.addEventListener("click", function(event){
-    if(!event.target.classList.contains("greyed")){
-        currentCard -= 1
-        updateCards();
-    }
-});
 
 
 rightArrow.addEventListener("click", function(event){
     if(!event.target.classList.contains("greyed")){
-        currentCard += 1
-        updateCards();
+        moveCards(1)
     }
 });
-
